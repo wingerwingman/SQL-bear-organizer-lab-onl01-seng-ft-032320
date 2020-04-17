c9ad3c9dcd4d21cd9bc548cb4446e5da6ad6e3b3
Justin Winger <justinwinger4@gmail.com>
2020-04-17 16:14:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,6 +2,16 @@ INSERT INTO bears(name, age, gender, color, temperament, alive)
 VALUES ("Mr. Chocolate", 2, "femail", "blue", "bad", true)
 INSERT INTO bears(name, age, gender, color, temperament, alive)
 VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
 
 
 Tabitha
