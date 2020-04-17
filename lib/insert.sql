cf1f32738ecc747f89b4a76a41cf9fd8c531dfef
Justin Winger <justinwinger4@gmail.com>
2020-04-17 16:15:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,7 +9,7 @@ VALUES ("Sergeant Brown", 2, "femail", "blue", "bad", true)
 INSERT INTO bears(name, age, gender, color, temperament, alive)
 VALUES ("Melissa", 2, "femail", "blue", "bad", true)
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
+VALUES ("Grinch", 2, "femail", "blue", "bad", true)
 INSERT INTO bears(name, age, gender, color, temperament, alive)
 VALUES ("Rowdy", 2, "femail", "blue", "bad", true)
 
