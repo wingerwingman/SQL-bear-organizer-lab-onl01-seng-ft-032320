71fa0dba89bb655302e3951f3ead4fffff073b52
Justin Winger <justinwinger4@gmail.com>
2020-04-17 16:14:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO bears
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 2, "femail", "blue", "bad", true)
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
